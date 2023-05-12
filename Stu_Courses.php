900769cfd152982356cb9ef0b5c86971b6bc2f66
jli522543 <67664289+jli522543@users.noreply.github.com>
2023-05-11 21:05:41 -0700

    Add files via upload

--- Stu_Courses.php
+++ Stu_Courses.php
@@ -1,29 +1,41 @@
 <html>
 	<body>
-
+	<h1>Course Info</h1>
 	<?php
-		// username and password need to be replaced by your username and password
-		// dbname is the same as your username
-
-		$link = mysqli_connect('mariadb', 'cs332u22', 'A6830Lqf','dbname');
+		$link = mysqli_connect('mariadb', 'cs332u22', 'A6830Lqf','cs332u22');
 		if (!$link) {
-		  die('Could not connect: ' . mysql_error());
+		  die('Could not connect: ');
 		  }
 
 		echo 'Connected successfully<p>';
 
-		$query = "SELECT SE.section_no, COUNT(*) As num_of_students, SE.section_room, SE.section_start, SE.section_end FROM Section SE, Enrollment E, Course C WHERE C.course_no =" 
-		. $_POST["course_no"]
-		. "AND SE.section_course = C.course_no AND E.en_section = SE.section_no GROUP BY E.section_no";
+		$course =  $_POST["course_no"];
+
+		$query = 
+			"SELECT SE.section_no, COUNT(*) As num_of_students, SE.section_room, SE.section_start, SE.section_end
+			FROM Section SE, Enrollment E, Course C
+			WHERE C.course_no = '$course'
+			AND SE.section_course = C.course_no
+			AND E.en_section = SE.section_no
+			GROUP BY E.en_section;";
+			
+
 		$result = $link->query($query);
 
-		$row=$result->fetch_assoc();
-		printf("Section Number: %s<br>\n", $row["section_no"]);
-		printf("Number of Students in Course: %s<br>\n", $row["num_of_students"]);//but do the count version
-		printf("Classroom: %s<br>\n", $row["section_room"]);
-		printf("Start time: %s<br>\n", $row["section_start"]);
-		printf("End time: %s<br>\n", $row["section_end"]);
-
+		if ($result->num_rows > 0) {
+			// output data of each row
+			while ($row = $result->fetch_assoc()) {
+				printf("Section Number: %s<br>\n", $row["section_no"]);
+				printf("Number of Students in Course: %s<br>\n", $row["num_of_students"]);//but do the count version
+				printf("Classroom: %s<br>\n", $row["section_room"]);
+				printf("Start time: %s<br>\n", $row["section_start"]);
+				printf("End time: %s<br>\n", $row["section_end"]);
+		
+				printf("------------------------------------------<br>\n");
+			}
+		} else {
+			echo "0 results";
+		}
 		$result->free_result();
 		$link->close();
 
--- Stu_History.php
+++ Stu_History.php
@@ -1,30 +1,43 @@
 <html>
-	<body>
 
+<body>
+	<h1>Student History</h1>
 	<?php
-		// username and password need to be replaced by your username and password
-		// dbname is the same as your username
+	$link = mysqli_connect('mariadb', 'cs332u22', 'A6830Lqf', 'cs332u22');
+	if (!$link) {
+		die('Could not connect: ');
+	}
 
-		$link = mysqli_connect('mariadb', 'cs332u22', 'A6830Lqf','dbname');
-		if (!$link) {
-		  die('Could not connect: ' . mysql_error());
-		  }
+	echo 'Connected successfully<p>';
 
-		echo 'Connected successfully<p>';
+	$student = $_POST["stu_cwid"];
+	$query = "SELECT C.course_title, E.en_grade
+	FROM Course C,
+		Enrollment E,
+		Section SE
+	WHERE E.en_student = '$student'
+		AND C.course_no = E.en_course
+	GROUP BY C.course_title;";
 
-		$query = "SELECT C.course_title, E.en_grade FROM Course C, Enrollment E, Section SE WHERE E.en_student =" 
-		. $_POST["en_student"]
-		. "AND SE.section_no = E.en_section AND C.course_no = SE.section_course GROUP BY C.course_title";
-		$result = $link->query($query);
 
-		$row=$result->fetch_assoc();
-		printf("Course: %s<br>\n", $row["course_title"]);
-		printf("Grade: %s<br>\n", $row["en_grade"]);
+	$result = $link->query($query);
 
-		$result->free_result();
-		$link->close();
+	if ($result->num_rows > 0) {
+		// output data of each row
+		while ($row = $result->fetch_assoc()) {
+			printf("Course: %s<br>\n", $row["course_title"]);
+			printf("Grade: %s<br>\n", $row["en_grade"]);
+			printf("------------------------------------------<br>\n");
+		}
+	} else {
+		echo "0 results";
+	}
+
+	$result->free_result();
+	$link->close();
 
 	?>
 
-	</body>
+</body>
+
 </html>
